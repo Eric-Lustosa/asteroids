f65234b24d22441369c2324a83fb243266ade974
Eric-Lustosa <eric.g.lustosa@gmail.com>
2025-03-29 19:22:38 +0100

    Implementado Player na tela e função de rotação

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
 from pygame import surface 
+from player import Player  
 
 
 def main():
@@ -12,14 +13,21 @@ def main():
     running = True
     relog = pygame.time.Clock()
     dt = 0
+    player = Player( SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
-        pygame.display.flip()
-        relog.tick(60)
+        # Get dt and limit FPS in one call
         dt = relog.tick(60) / 1000
+         # Update game state
+        player.update(dt)
+        # Draw everything
+        screen.fill((0, 0, 0))
+        player.draw(screen)
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
